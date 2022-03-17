e8cab0d152ebebf1fd44a0b319c7e7ad1222acfa
marcelotakayama <marcelo.takayama00@gmail.com>
2022-03-17 20:03:14 -0300

    first server creation

---
+++ main.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor iniciado em: http://localhost:${PORT}`);
+});
