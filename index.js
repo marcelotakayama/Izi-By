0857f15685adea6244eb9af78823997070332a16
marcelotakayama <marcelo.takayama00@gmail.com>
2022-03-24 15:27:46 -0300

    funçao GET itens funcionando

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const mysql = require("mysql");
+const express = require("express");
+var app = express();
+const bodyparser = require("body-parser");
+
+app.use(bodyparser.json());
 
 var mysqlConnection = mysql.createConnection({
     host: "localhost",
@@ -13,3 +18,14 @@ mysqlConnection.connect((err) => {
     else
         console.log("Deu ruim" + JSON.stringify(err, undefined, 2))
 });
+
+app.listen(3000, ()=>console.log("Servidor rodando na porta 3000"));
+
+app.get('/itens', (req, res)=>{
+    mysqlConnection.query("SELECT * FROM items", (err, rows, fields)=>{
+        if(!err)
+            console.log(rows);
+        else
+            console.log(err);
+    })
+})
